8d18c04f93f66d33a25b073cbc37409a0829e3fd
StarrkF <faymergen98@gmail.com>
2023-04-22 03:57:35 +0300

    conf for prod

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,7 @@ window.Popper = Popper;
 import 'bootstrap';
 import '../scss/app.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
+
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 import jQuery from 'jquery';
@@ -12,6 +13,8 @@ window.$ = jQuery;
 window.axios = axios;
 window.Chart = Chart;
 
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = 'http://dynamic-library.test/api/';
+window.axios.defaults.baseURL =  import.meta.env.DEV ? import.meta.env.VITE_LOCAL_API_BASE_URL : import.meta.env.VITE_API_BASE_URL;
+console.log(import.meta.env.DEV)
 
