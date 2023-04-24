e158e976e783fbb0bb64a354d4a805db31c73780
StarrkF <faymergen98@gmail.com>
2023-04-24 19:55:22 +0300

    added basic api security

---
+++ app/Http/Middleware/ApiKeyMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiKeyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = $request->header('X-Api-Key');
+        if ($apiKey !== config('auth.api_key')) {
+            return new JsonResponse(['status' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
